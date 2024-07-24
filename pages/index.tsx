5d8179b31a96756fb74a572d6931969bf0a789a6
Mansvini Aggarwal <coderspecial@gmail.com>
2024-07-25 04:19:26 +0530

    redirect to chats on going to index.js if logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import withAuth from "@/components/withAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(()=>{
+    router.push('/chats')
+  }, []);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome Inside!</h1>
